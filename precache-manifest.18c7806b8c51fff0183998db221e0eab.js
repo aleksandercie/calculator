05e7ae64278c15f2307b48f8ad05593d3a2d5165
Aleksander <aleksander@MacBook-Air-Aleksander.local>
2019-10-15 10:15:02 +0200

    Updates

---
+++ precache-manifest.18c7806b8c51fff0183998db221e0eab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d72790702688853b2e4b3a4448a048",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "1720852f63c5cbcbd37e",
+    "url": "/calculator/static/css/main.858b5f33.chunk.css"
+  },
+  {
+    "revision": "973262cf2c3987badedd",
+    "url": "/calculator/static/js/2.0d9c2ab9.chunk.js"
+  },
+  {
+    "revision": "1720852f63c5cbcbd37e",
+    "url": "/calculator/static/js/main.3d5ccb9c.chunk.js"
+  },
+  {
+    "revision": "4ed1cf36ed9e5a064592",
+    "url": "/calculator/static/js/runtime-main.b8dd65a4.js"
+  }
+]);
